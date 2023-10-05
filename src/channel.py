797a08ace077cb80d8d9ffe61fb8599bb25150b0
vertiporogov <133884681+vertiporogov@users.noreply.github.com>
2023-10-05 19:04:29 +0300

    Revert "fixed 4"
    This reverts commit c78037d71438d6e30ee93ad04d57e649fef47415.

--- src/channel.py
+++ src/channel.py
@@ -3,22 +3,23 @@ import json
 import os
 
 api_key: str = os.getenv('API-KEY-YouTube')
+# youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('API-KEY-YouTube')
+    # youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id):
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-
         self.__channel_id = channel_id
-        channel = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
-        self.title = channel['items'][0]['snippet']['title']
-        self.description = channel['items'][0]['snippet']['description']
-        self.url = channel['items'][0]['snippet']['thumbnails']['default']['url']
-        self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
-        self.video_count = channel['items'][0]['statistics']['videoCount']
-        self.view_count = channel['items'][0]['statistics']['viewCount']
+        self.title = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()['items'][0]['snippet']['title']
+        self.description = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()['items'][0]['snippet']['description']
+        self.url = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()['items'][0]['snippet']['thumbnails']['default']['url']
+        self.subscriber_count = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()['items'][0]['statistics']['videoCount']
+        self.view_count = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -34,19 +35,21 @@ class Channel:
     @classmethod
     def get_service(cls):
         """Метод возвращающий объект для работы с YouTube API"""
+        youtube_ = build('youtube', 'v3', developerKey=api_key)
 
-        return build('youtube', 'v3', developerKey=api_key)
+        return youtube_
 
     def to_json(self, data_file):
         """Метод сохраняющий в файл значения атрибутов экземпляра"""
-        data = {
-            "title": self.title,
-            "description": self.description,
-            "url": self.url,
-            "subscriber_count": self.subscriber_count,
-            "video_count": self.video_count,
-            "view_count": self.view_count
-        }
-
+        data_list = []
         with open(data_file, "w", encoding='utf-8') as jsonfile:
-            json.dump(data, jsonfile, ensure_ascii=False, indent=4)
+            # fff.append(self.channel_id)
+            data_list.append(self.title)
+            data_list.append(self.description)
+            data_list.append(self.url)
+            data_list.append(self.subscriber_count)
+            data_list.append(self.video_count)
+            data_list.append(self.view_count)
+
+            return jsonfile.write(json.dumps(data_list))
+
