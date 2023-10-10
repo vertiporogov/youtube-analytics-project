3bed25ca48d554fcc2da9d7eabfe6b9e183924f0
vertiporogov <133884681+vertiporogov@users.noreply.github.com>
2023-10-10 19:22:36 +0300

    home_work_15.1 finish 2

--- src/video.py
+++ src/video.py
@@ -1,5 +1,3 @@
-import json
-
 from googleapiclient.discovery import build
 import os
 
@@ -7,8 +5,6 @@ api_key: str = os.getenv('API-KEY-YouTube')
 
 
 class Video:
-
-    # you_tube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id: str):
         self.__video_id = video_id
@@ -31,20 +27,20 @@ class Video:
 
         return build('youtube', 'v3', developerKey=api_key)
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале ."""
-        video_id = self.video_id
-        channel = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
-        return print(json.dumps(channel, indent=2, ensure_ascii=False))
-
 
 class PLVideo(Video):
 
     def __init__(self, video_id: str, playlist_id: str):
         super().__init__(video_id)
-        self.playlist_id = playlist_id
+        self.__playlist_id = playlist_id
         channel = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
         self.title = channel['items'][0]['snippet']['title']
         self.url = channel['items'][0]['snippet']['thumbnails']['default']['url']
         self.view_count = channel['items'][0]['statistics']['viewCount']
         self.like_count = channel['items'][0]['statistics']['likeCount']
+
+    @property
+    def playlist_id(self):
+        return self.__playlist_id
+
+
